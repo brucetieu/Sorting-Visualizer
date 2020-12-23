5ec502ead0149513f31ebe566bcd39bbc4917ca6
brucetieu <brucetieu5@gmail.com>
2020-12-22 19:38:52 -0700

    Add comments

--- Sorting-Algos/BubbleSort.js
+++ Sorting-Algos/BubbleSort.js
@@ -3,11 +3,14 @@ const SWAP_COLOR = "lightcoral";
 const PRE_SWAP_COLOR = "lightblue";
 const FINAL_SWAP_COLOR = "orange";
 
+const randArrBubble = document.getElementById("rand_arr");
+
 /**
  * Bubble sort function. Returns a promise since we have 'async'.
  * @param {*} childnodes The div child nodes to be swapped. 
  */
 async function bubbleSort(childnodes) {
+    rand_arr.disabled = true;
     let size = childnodes.length;
 
     let swapped;
@@ -53,19 +56,20 @@ async function bubbleSort(childnodes) {
         childnodes[i].style.backgroundColor = PRE_SWAP_COLOR;
     }
 
-    sortButton.disabled = false;
+    sortButtonBubble.disabled = false;
+    randArrBubble.disabled = false;
 }
 
-const sortButton = document.getElementById("btn");
-const option = document.getElementById("sorting-ctrl");
+let sortButtonBubble = document.getElementById("btn");
+let optionBubble = document.getElementById("sorting-ctrl");
 
 // Perform animation when sort button is clicked.
-sortButton.addEventListener("click", function() {
+sortButtonBubble.addEventListener("click", function() {
 
     if (canvas.getDiv == null) return; 
 
-    if (option.value == BUBBLE) {
-        sortButton.disabled = true;
+    if (optionBubble.value == BUBBLE) {
+        sortButtonBubble.disabled = true;
         bubbleSort(canvas.getDiv.childNodes);
     }
 
