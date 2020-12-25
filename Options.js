bc359ec5c2475d7e1f494f13e94cd1b0965631e6
brucetieu <brucetieu5@gmail.com>
2020-12-24 18:52:13 -0700

    Add Options.js

---
+++ Options.js
@@ -0,0 +1,17 @@
+var SORTING_ALGORITHM = {
+    BUBBLE_SORT: "bubble_sort",
+    INSERTION_SORT: "insertion_sort",
+    SELECTION_SORT: "selection_sort"
+};
+
+var COLORS = {
+    SWAP_COLOR: "lightcoral",
+    PRE_SWAP_COLOR: "lightblue",
+    FINAL_SWAP_COLOR: "orange",
+    MIN_ELEMENT_COLOR: "lightgreen"
+};
+
+var HEIGHTS = {
+    MIN_HEIGHT: "10000px",
+    MIN_TEXT: "10000px"
+}
