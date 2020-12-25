68983569fa7a459f847247197d6da63c954ed8e7
brucetieu <brucetieu5@gmail.com>
2020-12-25 10:33:11 -0700

    Separated out swapping function

--- Sorting-Algos/BubbleSort.js
+++ Sorting-Algos/BubbleSort.js
@@ -1,5 +1,6 @@
 
 import {sortButton, randArr} from '../Animation.js';
+import {swap} from './Swap.js';
 /**
  * Bubble sort function. Returns a promise since we have 'async'.
  * @param {*} childnodes The div child nodes to be swapped. 
@@ -16,33 +17,14 @@ async function bubbleSort(childnodes) {
         { 
             if (parseInt(childnodes[j].style.height) > parseInt(childnodes[j+1].style.height))  
             { 
-
-                childnodes[j].style.backgroundColor = COLORS.SWAP_COLOR;
-                childnodes[j+1].style.backgroundColor = COLORS.SWAP_COLOR;
-
-                let temp = childnodes[j].style.height
-                childnodes[j].style.height = childnodes[j+1].style.height; 
-                childnodes[j+1].style.height = temp;
-                
-                let temp2 = childnodes[j].textContent;
-                childnodes[j].textContent = childnodes[j+1].textContent;
-                childnodes[j+1].textContent = temp2
-                
-                let slider = document.getElementById("customRange1");
-                
-                await delay(slider.value);
-
-                childnodes[j].style.backgroundColor = COLORS.PRE_SWAP_COLOR;
-                childnodes[j+1].style.backgroundColor = COLORS.PRE_SWAP_COLOR;
-            
+                await swap(childnodes[j], childnodes[j+1]);
                 swapped = true; 
             }
 
         } 
         childnodes[childnodes.length - 1 - i].style.backgroundColor = COLORS.FINAL_SWAP_COLOR;
 
-        // IF no two elements were  
-        // swapped by inner loop, then break 
+        // IF no two elements were swapped by inner loop, then break. 
         if (swapped == false) {
             break; 
         }
@@ -50,7 +32,7 @@ async function bubbleSort(childnodes) {
     for (let i = 0; i < size; i++) {
         childnodes[i].style.backgroundColor = COLORS.PRE_SWAP_COLOR;
     }
-
+    
     sortButton.disabled = false;
     randArr.disabled = false;
 }
