2f73925e35220439d2fa3f669a161c9073e137da
brucetieu <brucetieu5@gmail.com>
2020-12-25 11:38:31 -0700

    Add ShellSort.js

---
+++ Sorting-Algos/ShellSort.js
@@ -0,0 +1,30 @@
+import {sortButton, randArr} from '../Animation.js';
+import {swap} from './Swap.js';
+
+// Source: Algorithms 4th Edition - Sedgewick
+async function shellSort(childnodes) {
+    randArr.disabled = true;
+
+    let N = childnodes.length;
+    let h = 1;
+
+    // Use Knuth's 3x + 1 increment sequence.
+    while (h < Math.floor(N/3)) {
+        h = 3*h + 1;
+    }
+
+    // h-sort the array.
+    while (h >= 1) {
+        for (let i = h; i < N; i++) {
+            for (let j = i; j >= h && parseInt(childnodes[j].style.height) < parseInt(childnodes[j-h].style.height); j -= h) {
+                console.log(i, j, h);
+                await swap(childnodes[j], childnodes[j-h]);
+            }
+        }
+        h = Math.floor(h/3);
+    }
+    sortButton.disabled = false;
+    randArr.disabled = false;
+}
+
+export {shellSort};
