3a9a94f0a783c2f4b7acac20a77c692d8eefff63
brucetieu <brucetieu5@gmail.com>
2020-12-25 10:33:35 -0700

    Separated out swapping mechanism

--- Sorting-Algos/InsertionSort.js
+++ Sorting-Algos/InsertionSort.js
@@ -1,27 +1,5 @@
-
-
-
 import {sortButton, randArr} from '../Animation.js';
-
-async function _swap(block1, block2) {
-    block2.style.backgroundColor = COLORS.SWAP_COLOR;
-    block1.style.backgroundColor = COLORS.MIN_ELEMENT_COLOR;
-    // block2.style.backgroundColor = SWAP_COLOR;
-
-    let sliderInsertion = document.getElementById("customRange1");
-    await delay(sliderInsertion.value);
-
-    let tempHeight = block1.style.height
-    block1.style.height = block2.style.height;
-    block2.style.height = tempHeight;
-
-    let tempText = block1.textContent
-    block1.textContent = block2.textContent;
-    block2.textContent = tempText;
-
-    block1.style.backgroundColor = COLORS.PRE_SWAP_COLOR;
-    block2.style.backgroundColor = COLORS.PRE_SWAP_COLOR;
-}
+import {swap} from './Swap.js';
 
 async function insertionSort(childnodes) {
     randArr.disabled = true;
@@ -31,7 +9,7 @@ async function insertionSort(childnodes) {
 
         while (j > 0 && parseInt(childnodes[j-1].style.height) > parseInt(childnodes[j].style.height)) {
 
-            await _swap(childnodes[j], childnodes[j-1]);
+            await swap(childnodes[j], childnodes[j-1]);
             j--;
         }
 
