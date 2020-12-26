7f18decdea058f5bd2e707d84991a492e9be7316
brucetieu <brucetieu5@gmail.com>
2020-12-26 11:41:27 -0700

    Disable buttons upon clicking sort

--- Sorting-Algos/QuickSort.js
+++ Sorting-Algos/QuickSort.js
@@ -6,13 +6,19 @@ import {swap} from './Swap.js';
 
 
 async function quickSort(childNodes) {
-
     await _qs(childNodes, 0, childNodes.length - 1);
 }
 
 async function _qs(arr, l, r) {
-    if (l >= r) return;
+    if (l >= r) {
+        randArr.disabled = false;
+        sortButton.disabled = false;
+        return;
+    }
 
+    sortButton.disabled = true;
+    randArr.disabled = true;
+    
     let p = await partition(arr, l, r);
     await _qs(arr, l, p-1);
     await _qs(arr, p+1, r);
@@ -30,31 +36,16 @@ async function partition(arr, l, r) {
         // arr[r].style.backgroundColor = "#9370DB";
         if (parseInt(arr[j].style.height) < parseInt(pivot)) {
             i++;
+            if (j == r) {
+                arr[j].style.backgroundColor = "#9370DB";
+            }
             await swap(arr[i], arr[j]);
+
         }
     }
     // Swap pivot with position i + 1.
     await swap(arr[i+1], arr[r]); 
-
-    // arr[i+1].style.backgroundColor = COLORS.SWAP_COLOR;
-    // arr[r].style.backgroundColor = COLORS.COMPARE_COLOR;
-
-    // let sliderInsertion = document.getElementById("customRange1");
-    // await delay(sliderInsertion.value);
-
-    // let tempHeight = arr[i+1].style.height
-    // arr[i+1].style.height = arr[r].style.height;
-    // arr[r].style.height = tempHeight;
-
-    // let tempText = arr[i+1].textContent
-    // arr[i+1].textContent = arr[r].textContent;
-    // arr[r].textContent = tempText;
-
-    // arr[i+1].style.backgroundColor = COLORS.PRE_SWAP_COLOR;
-    // arr[r].style.backgroundColor = COLORS.PRE_SWAP_COLOR;
-
     return i + 1;
-    
 
 }
 
