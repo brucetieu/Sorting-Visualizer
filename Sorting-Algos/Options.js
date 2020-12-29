d6c2b508655ba9554dab62fa0513abbb681ec85d
brucetieu <brucetieu5@gmail.com>
2020-12-29 10:00:11 -0700

    Add comments

--- Sorting-Algos/Options.js
+++ Sorting-Algos/Options.js
@@ -1,3 +1,7 @@
+
+/**
+ * Sorting algorithm options.
+ */
 var SORTING_ALGORITHM = {
     BUBBLE_SORT: "bubble_sort",
     INSERTION_SORT: "insertion_sort",
@@ -7,13 +11,20 @@ var SORTING_ALGORITHM = {
     MERGESORT: "mergesort"
 };
 
+/**
+ * Options for the color of the divs.
+ */
 var COLORS = {
     SWAP_COLOR: "lightcoral",
     PRE_SWAP_COLOR: "lightblue",
     FINAL_SWAP_COLOR: "orange",
-    COMPARE_COLOR: "lightgreen"
+    COMPARE_COLOR: "lightgreen",
+    PIVOT: "#9370DB"
 };
 
+/**
+ * Minimum heights for the divs (used in selection sort).
+ */
 var HEIGHTS = {
     MIN_HEIGHT: "10000px",
     MIN_TEXT: "10000px"
